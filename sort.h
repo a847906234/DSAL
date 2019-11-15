03c7b851d954e3fbfa8ab511b1b9e5e628ca4823
cnchen2000 <cnccnu@163.com>
2019-11-15 17:29:05 +0800

    quicksort

---
+++ sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+int partition(int a[], int p, int q);
+void quickSort(int a[], int n);
+void printArray(int a[], int n);
+
+#endif
--- test.c
+++ test.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#include "sort.h"
+
+#define N 8
 int main(int argc, char const *argv[])
 {
-    printf("Haha\n");
+    int a[N] = {6, 10, 13, 5, 8, 3, 2, 11};
+    quickSort(a, N);
+
+    printArray(a, N);
+
     return 0;
 }
